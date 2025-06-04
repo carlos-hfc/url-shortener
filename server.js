24a035fd38f89e4eecf9d86b3fb5ee0fbeecceab
Carlos Faustino <chfcchfc96@gmail.com>
2025-06-04 09:10:16 -0300

    feat: express encoded

--- server.js
+++ server.js
@@ -7,11 +7,13 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 let db = []
 let id = 1
 
 app.post("/api/shorturl", async (request, response) => {
+  console.log(request.body)
   const url = request.body.url
   const domain = urlParser.parse(url)
 
